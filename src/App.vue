d321254c7cbb901434bd28b4069c119fc936601b
Diana <vyalova.diana@yandex.ru>
2022-12-29 17:23:08 +0300

    Manual Two-Way binding

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <post-form/>
+    <post-list :posts="posts"/>
+
+
+  </div>
 </template>
 
+<script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+export default {
+  components: {
+    PostList,PostForm
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: "Пост о javascript", body: "Javascript - язык программирования"},
+        {id: 2, title: "Пост о javascript 2", body: "Javascript - язык программирования 2"},
+        {id: 3, title: "Пост о javascript 3", body: "Javascript - язык программирования 3"}
+      ],
+      title: "",
+      body: ""
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost);
+      this.title="";
+      this.body = "";
+    },
+
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+
+
+
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input @input="post.title = $event.target.value" :value="post.title" class="input" type="text" placeholder="Название">
+    <input @input="post.body = $event.target.value" :value="post.body" class="input" type="text" placeholder="Описание">
+    <button class="btn">Создать</button>
+<!--    <button class="btn" @click="createPost">Создать</button>-->
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post:{
+        title:"",
+        body:"",
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+
+}
+</style>
