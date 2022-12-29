f16f410dd2180fbe8530c541b7e53ee963f423c0
Diana <vyalova.diana@yandex.ru>
2022-12-29 22:56:45 +0300

    Added 'delete post' option

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <post-form @create="createPost"/>
-    <post-list :posts="posts" />
+    <post-list :posts="posts" @remove="removePost"/>
 
 
   </div>
@@ -24,8 +24,11 @@ export default {
     };
   },
   methods: {
-    createPost(post,){
+    createPost(post){
       this.posts.push(post);
+    },
+    removePost(post){
+      this.posts= this.posts.filter(p => p.id !== post.id )
     }
 
   }
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
+  <div v-if="posts.length !== 0">
     <h3>Список постов</h3>
-    <post-item v-for="post in posts" :post="post"/>
+    <post-item v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove',post)"/>
+    <!--
+    1)props can't be altered, should throw event to upper component
+    2)v-show could be used instead of v-if to save element in DOM (display:none)
+    -->
   </div>
-
+  <h2 v-else style="color: red">Постов нет! </h2>
 </template>
 
 <script>
