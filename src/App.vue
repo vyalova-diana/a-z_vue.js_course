055cb60cddd1f48573df45647aa727a4200550d9
Diana <vyalova.diana@yandex.ru>
2023-01-05 22:05:59 +0300

    Added dialog window containing create form

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
-    <post-form @create="createPost"/>
+    <h1>Страница с постами</h1>
+    <my-button @click="showDialog" style="margin: 15px 0;">Создать пост</my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost"/>
+    </my-dialog>
     <post-list :posts="posts" @remove="removePost"/>
 
 
@@ -10,8 +14,12 @@
 <script>
 import PostForm from "@/components/PostForm.vue";
 import PostList from "@/components/PostList.vue";
+import MyDialog from "@/components/UI/MyDialog.vue";
+import MyButton from "@/components/UI/MyButton.vue";
 export default {
   components: {
+    MyButton,
+    MyDialog,
     PostList,PostForm
   },
   data() {
@@ -21,14 +29,19 @@ export default {
         {id: 2, title: "Пост о javascript 2", body: "Javascript - язык программирования 2"},
         {id: 3, title: "Пост о javascript 3", body: "Javascript - язык программирования 3"}
       ],
+      dialogVisible: false
     };
   },
   methods: {
     createPost(post){
       this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post){
       this.posts= this.posts.filter(p => p.id !== post.id )
+    },
+    showDialog() {
+      this.dialogVisible = true;
     }
 
   }
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,6 +1,8 @@
 import myButton from "@/components/UI/MyButton.vue";
 import myInput from "@/components/UI/MyInput.vue";
+import myDialog from "@/components/UI/MyDialog.vue";
 export default [
     myButton,
     myInput,
+    myDialog
 ]
