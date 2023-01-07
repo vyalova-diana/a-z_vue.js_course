0b45d6c246cc3f7b41c04d382da1cbf5e2c7a299
Diana <vyalova.diana@yandex.ru>
2023-01-07 22:51:00 +0300

    Added list animation via <transition-group>

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,14 @@
 <template>
   <div v-if="posts.length !== 0">
     <h3>Список постов</h3>
-    <post-item v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove',post)"/>
-    <!--
-    1)props can't be altered, should throw event to upper component
-    2)v-show could be used instead of v-if to save element in DOM (display:none)
-    -->
+    <transition-group name="post-list">
+      <post-item v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove',post)"/>
+      <!--
+      1)props can't be altered, should throw event to upper component
+      2)v-show could be used instead of v-if to save element in DOM (display:none)
+      -->
+    </transition-group>
+
   </div>
   <h2 v-else style="color: red">Постов нет! </h2>
 </template>
@@ -25,5 +28,21 @@ export default {
 </script>
 
 <style scoped>
+.post-list-move,
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
 
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.post-list-leave-active {
+  position: absolute;
+}
 </style>
