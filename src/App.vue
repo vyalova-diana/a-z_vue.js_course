7fdc32e27e8c91cc0661309dd0fb88f088fe6111
Diana <vyalova.diana@yandex.ru>
2023-01-09 00:06:48 +0300

    TO-DO: infinite scroll. IntersectionalObserver and v-if conflict

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
+
     <my-input v-model="searchQuery" placeholder="Поиск по названию"/>
     <div class="app__btns">
       <my-button @click="showDialog" >Создать пост</my-button>
@@ -9,13 +10,15 @@
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost"/>
     </my-dialog>
-    <post-list v-if="!isPostsLoading" :posts="searchAndSortedPosts" @remove="removePost"/>
-    <h2 v-else style="color: yellow">Загрузка... </h2>
     <div class="page__wrapper">
       <div class="page" v-for="pageNumber in totalPages" :key="pageNumber" :class="{'current-page': pageNumber === page}" @click="changePage(pageNumber)">
         {{pageNumber}}
       </div>
     </div>
+    <post-list v-if="!isPostsLoading" :posts="searchAndSortedPosts" @remove="removePost"/>
+    <h2 v-else style="color: yellow">Загрузка... </h2>
+    <div v-if="!isPostsLoading" ref="observer" class="observer">observer</div>
+
   </div>
 </template>
 
@@ -83,12 +86,44 @@ export default {
         this.isPostsLoading = false;
       }
     },
+    async fetchMorePosts() {
+      try {
+        this.page += 1;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
+        this.posts = [...this.posts, ...response.data];
+      }
+      catch (e) {
+        alert("Ошибка запроса!")
+      }
+
+    },
     changePage(pageNumber) {
       this.page = pageNumber;
+      this.fetchPosts();
     }
   },
   mounted() {
     this.fetchPosts();
+
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0
+    };
+    const callback = (entries, observer) => {
+      console.log(entries[0].isIntersecting);
+      if (entries[0].isIntersecting && this.page < this.totalPages){
+        this.fetchMorePosts();
+      }
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
@@ -99,9 +134,7 @@ export default {
     }
   },
   watch: {
-    page() {
-      this.fetchPosts();
-    }
+
   }
 };
 </script>
@@ -133,5 +166,8 @@ export default {
 }
 .current-page {
   border: 2px solid teal;
+}
+.observer {
+  height:30px;
 }
 </style>
