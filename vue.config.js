22009463a7a620faa28f5ae0427a9110208794d5
Diana <vyalova.diana@yandex.ru>
2023-02-06 21:29:23 +0300

    Added deploy config files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/a-z_vue.js_course/'
+      : '/'
 })
